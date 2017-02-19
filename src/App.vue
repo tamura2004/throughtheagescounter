aab7aa2bdb3367eeef639d569599bfa99d8f8dae
tamura <tamura2004@gmail.com>
2017-02-19 20:56:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+.container
+  .row
+    .col-sm-3
+      router-link.btn.btn-warning(to="/")
+        p.card-text 文化ポイント
+        h4.card-title {{users[0].science.current}}
+
+</template>
+
+<script>
+export default {
+  name: 'calc',
+  data () {
+    return {
+      users: [
+        {
+          name: '田村',
+          color: 'btn-danger',
+          science: {
+            current: 0,
+            increase: 1
+          },
+          culture: {
+            current: 0,
+            increase: 0
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 20px
+
+</style>
